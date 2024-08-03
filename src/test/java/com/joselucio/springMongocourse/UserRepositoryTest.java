d32718e823b640bf645b3d4d04588b222c8d88a9
Jose Lucio <jloliveirajunior1903@gmail.com>
2024-08-03 14:22:55 -0300

    Users update,delete and findByName tests implementation

--- src/test/java/com/joselucio/springMongocourse/UserRepositoryTest.java
+++ src/test/java/com/joselucio/springMongocourse/UserRepositoryTest.java
@@ -1,6 +1,9 @@
 package com.joselucio.springMongocourse;
 
 
+import java.util.List;
+import java.util.Optional;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,14 +21,75 @@ class UserRepositoryTest {
 	private UserRepository userRepository;
 	
 	@Test
-	@DisplayName("Save creates User when sucessful")
+	@DisplayName("Save persists User when sucessful")
 	void save_PersistUser_WhenSucessful() {
 		LinkedinUser userToBeSaved = createUser();
 		LinkedinUser userSaved = this.userRepository.save(userToBeSaved);
+		
 		Assertions.assertThat(userSaved).isNotNull();
+		
 		Assertions.assertThat(userSaved.getId()).isNotNull();
+		
 		Assertions.assertThat(userSaved.getName()).isEqualTo(userToBeSaved.getName());
 		
+		
+	}
+	
+	@Test
+	@DisplayName("Save updates User when sucessful")
+	void save_UpdatesUser_WhenSucessful() {
+		LinkedinUser userToBeSaved = createUser();
+		LinkedinUser userSaved = this.userRepository.save(userToBeSaved);
+		
+		userSaved.setName("Peter Crouch");
+		
+		LinkedinUser userUpdated = this.userRepository.save(userSaved);
+		
+		Assertions.assertThat(userUpdated).isNotNull();
+		
+		Assertions.assertThat(userUpdated.getId()).isNotNull();
+		
+		Assertions.assertThat(userUpdated.getName()).isEqualTo(userSaved.getName());
+		
+		
+	}
+	
+	@Test
+	@DisplayName("Delete removes User when sucessful")
+	void delete_RemovesUser_WhenSucessful() {
+		LinkedinUser userToBeSaved = createUser();
+		LinkedinUser userSaved = this.userRepository.save(userToBeSaved);
+		
+		this.userRepository.delete(userSaved);
+		
+		Optional<LinkedinUser> userOptional = this.userRepository.findById(userSaved.getId());
+		
+		Assertions.assertThat(userOptional).isEmpty();
+	}
+
+	@Test
+	@DisplayName("Find By Name returns list of User when sucessful")
+	void findByName_ReturnsListOfUser_WhenSucessful() {
+		LinkedinUser userToBeSaved = createUser();
+		LinkedinUser userSaved = this.userRepository.save(userToBeSaved);
+		
+		String name = userSaved.getName();
+		
+		List<LinkedinUser> users = this.userRepository.findByName(name);
+		
+		Assertions.assertThat(users).isNotEmpty();
+		
+		Assertions.assertThat(users).contains(userSaved);
+	}
+	
+
+	@Test
+	@DisplayName("Find By Name returns empty list when user is not found")
+	void findByName_ReturnsEmptyList_WhenUserIsNotFound() {
+		
+		List<LinkedinUser> users = this.userRepository.findByName("xx");
+		
+		Assertions.assertThat(users).isEmpty();
 		
 	}
 	
